ee33d20a473473b8442dbc53b3ef8ace421f88a4
Peter Tynan <pettynan@amazon.com>
2019-06-17 12:30:03 -0700

    lab complete

--- 401-code-challenges/src/test/java/tree/BinaryTreeTest.java
+++ 401-code-challenges/src/test/java/tree/BinaryTreeTest.java
@@ -3,10 +3,6 @@ package tree;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-
 import static org.junit.Assert.*;
 
 public class BinaryTreeTest {
